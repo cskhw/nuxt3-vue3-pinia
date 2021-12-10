f2efc1e69d54781150f332634d04808051adf3a3
khw0828 <gusdn0828@gmail.com>
2021-12-10 22:05:03 +0900

    before install vuetify

--- app.vue
+++ app.vue
@@ -7,6 +7,7 @@
     <button @click="check">check</button>
     <button @click="getCount">getCount</button>
   </div>
+  <nuxt-link to="/about">About</nuxt-link>
   <div>
     <h1>pinia subscribe example</h1>
     <p>{{ counterStore.$state }}</p>
@@ -15,26 +16,28 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
-import useCounterStore from '@/stores/counter'
+import { defineComponent } from "vue";
+import useCounterStore from "@/stores/counter";
 
 export default defineComponent({
+  components: {},
   setup() {
-    const counterStore = useCounterStore()
+    const counterStore = useCounterStore();
     const increAction = () => counterStore.counter++;
-    const increPatch = () => counterStore.$patch({ counter: counterStore.counter + 1 })
+    const increPatch = () =>
+      counterStore.$patch({ counter: counterStore.counter + 1 });
     const check = () => console.log(counterStore.counter);
     const getCount = () => counterStore.getCounter;
     counterStore.$subscribe((mutation, state) => {
-      console.log(state)
-    })
+      console.log(state);
+    });
     return { counterStore, check, increAction, getCount, increPatch };
   },
-})
+});
 </script>
 <style lang="scss">
-@import './assets/design';
-button + button{
+@import "./assets/design";
+button + button {
   margin-left: 1vw;
 }
 </style>
---
+++ components/common/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="common-button">
+    <button>asdf</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  components: {},
+  props: {},
+  setup(props) {
+    return { props };
+  },
+});
+</script>
+
+<style></style>
